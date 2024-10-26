4d99fa0a6c8f10daab4baf0f4dc0b9ca7681a81b
saadkhan4 <saadshaheen1327@gmail.com>
2024-10-26 22:04:46 +0500

    algorithms in process

--- Class-1/search-string.c
+++ Class-1/search-string.c
@@ -19,3 +19,21 @@ int main(void)
     printf("Not found\n");
     return 1;
 }
+
+
+int main(void)
+{
+    string games[] = {"Resident evil", "Fort-nite", "Cyberpunk", "Rivals", "Spider-man"};
+
+    string s = get_string("Objects: ");
+    for (int i = 0; i < 5; i++)
+    {
+        if (games[i] , s)
+        {
+            printf("found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
--- Class-1/search.c
+++ Class-1/search.c
@@ -20,3 +20,20 @@ int main(void)
     printf("Not found\n");
     return 1;
 }
+
+int main(void)
+{
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+    int n = get_int("Number: ");
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
