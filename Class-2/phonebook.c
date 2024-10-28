77af7b609c77f1987b04f96958cfcf407c4580c3
saadkhan4 <saadshaheen1327@gmail.com>
2024-10-28 23:12:07 +0500

    using string in algorithms

---
+++ Class-2/phonebook.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Thomas", "Charlotte", "Blake"};
+    string numbers[] = {"0345-6153807", "0319-3346494", "0319-6744555"};
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("found %s\n" , numbers[i]);     // we are using placeholder this time because we aren't just printing number but we're printing names as well//
+            return 0;
+        }
+    }
+    printf("not found\n");
+    return 1;
+}
