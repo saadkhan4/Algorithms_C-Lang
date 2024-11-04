6dbd90a390c89466e8c47baddb37f3768f1c5279
saadkhan4 <saadshaheen1327@gmail.com>
2024-11-04 23:42:20 +0500

    Practice of previous classes

--- Class-1/search-string.c
+++ Class-1/search-string.c
@@ -10,30 +10,16 @@ int main(void)
     string s = get_string("Objects: ");
     for (int i = 0; i < 6; i++)
     {
-        if (strcmp(objects[i], s) == 0)             
-        {
-            printf("Found\n");
-            return 0;
-        }
+        if (strcmp(objects[i], s) == 0)         ////strcmp(objects[i], s): This function compares two strings (objects[i] and s).
+                                                // • It returns 0 if the strings are exactly the same.
+	                                            // • == 0 : The if condition checks if the result of strcmp is 0, meaning objects[i] and s are equal.
+            {
+                printf("Found\n");
+                return 0;
+            }
     }
     printf("Not found\n");
     return 1;
 }
 
 
-int main(void)
-{
-    string games[] = {"Resident evil", "Fort-nite", "Cyberpunk", "Rivals", "Spider-man"};
-
-    string s = get_string("Objects: ");
-    for (int i = 0; i < 5; i++)
-    {
-        if (games[i] , s)
-        {
-            printf("found\n");
-            return 0;
-        }
-    }
-    printf("Not found\n");
-    return 1;
-}
--- Class-1/search.c
+++ Class-1/search.c
@@ -4,12 +4,30 @@
 //// correct implementation of linear search////
 /// from left to right, looking for a number that may or may not be there.///
 
-int main(void)              
+// int main(void)              
+// {
+//     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+//     int n = get_int("Number: ");
+//     for (int i = 0; i < 7; i++)
+//     {
+//         if (numbers[i] == n)
+//         {
+//             printf("found\n");
+//             return 0;
+//         }
+//     }
+//     printf("Not found\n");
+//     return 1;
+// }
+
+int main (void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    const int N = 5;
+    int numbers[N] = {20, 30, 49, 89, 90};
 
     int n = get_int("Number: ");
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < N; i++)
     {
         if (numbers[i] == n)
         {
@@ -17,23 +35,6 @@ int main(void)
             return 0;
         }
     }
-    printf("Not found\n");
-    return 1;
-}
-
-int main(void)
-{
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
-
-    int n = get_int("Number: ");
-    for (int i = 0; i < 7; i++)
-    {
-        if (numbers[i] == n)
-        {
-            printf("found\n");
-            return 0;
-        }
-    }
-    printf("Not found\n");
+    printf("Not Found\n");
     return 1;
 }
