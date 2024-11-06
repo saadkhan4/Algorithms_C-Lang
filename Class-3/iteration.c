94697087dcceaab2e7e83ab0ff602cf7b902831a
saadkhan4 <saadshaheen1327@gmail.com>
2024-11-06 21:52:38 +0500

    practice of iteration

--- Class-3/iteration.c
+++ Class-3/iteration.c
@@ -3,6 +3,27 @@
 
 void draw(int n);
 
+// int main(void)
+// {
+//     int height = get_int("Height: ");
+//     draw(height);
+// }
+
+// void draw(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i + 1; j++)   //we just have need to one hash one and after && after thats why we add + 1 //
+//         {
+//             printf("#");
+//         }
+
+//         printf("\n");
+//     }
+
+// }
+
+
 int main(void)
 {
     int height = get_int("Height: ");
@@ -11,19 +32,12 @@ int main(void)
 
 void draw(int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int e = 0; e < n; e++)
     {
-        for (int j = 0; j < i + 1; j++)   //we just have need to one hash one and after && after//
+        for (int p = 0; p < e + 1; p++)   
         {
-            printf("#");
+            printf("$");
         }
-
         printf("\n");
     }
-
-}
-
-
-int main(void){
-    
 }
